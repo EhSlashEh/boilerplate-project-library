0bcb85264a13e6840ad7f51eda1ed422f23c6ffe
EhSlashEh <anthonyalanb@hotmail.com>
2024-08-23 14:12:09 -0300

    bye uri

--- db-connection.js
+++ db-connection.js
@@ -2,12 +2,7 @@ const mongoose = require("mongoose");
 
 const uri = process.env.DB;
 
-if (!uri) {
-    console.error("MongoDB URI is not defined in the environment variables.");
-    process.exit(1);
-  }
-
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri)
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => {
     console.error("Failed to connect to MongoDB:", err);
